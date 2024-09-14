7259a47e3ea4ec95c2dc5be1a7119def27120ab0
camilleli <camillekamiu@gmail.com>
2024-09-14 10:43:28 +0930

    Adding first 3 script

---
+++ term_by_user_agent.js
@@ -0,0 +1,107 @@
+function stepTimeout(actions, delay) {
+    actions.forEach((action, index) => {
+        setTimeout(action, delay * (index + 1));
+    });
+}
+
+function setInputValue(inputElement, newValue) {
+  // write client ip in the input
+  const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+  nativeInputValueSetter.call(inputElement, newValue);
+  // Dispatch the input event to trigger React's state update
+  const event = new Event('input', { bubbles: true });
+  inputElement.dispatchEvent(event);
+}
+
+// Define the actions as functions
+var actions = [
+      //----
+      //#### Horizontal axis
+    () => {
+      // edit Horizontal axis
+      $('fieldset').has('legend:contains("Horizontal axis")').find('button[title^="Edit"][title$="configuration"]').click();
+    },
+    () => {
+      // click date histogram
+      $("button").has('span[title="Date histogram"]').click();
+      },
+    () => {
+      // click field
+      $('div[data-test-subj="indexPattern-dimension-field"]').find('input').click();
+      },
+    () => {
+      // click timestamp in drop down
+      $(".euiPopover__panel").find('button.euiFilterSelectItem[title="@timestamp"]').click();
+      },
+    () => {
+      // close popup
+      $('span.euiButtonEmpty__text:contains("Close")').closest('button').click()
+      },
+  
+
+      //----
+      //#### Vertical axis
+    () => {
+      //edit vertical axis
+      $('fieldset').has('legend:contains("Vertical axis")').find('button[title^="Edit"][title$="configuration"]').click();
+      },
+    () => {
+      // click forumla button
+      $('span[data-text="Quick function"]').closest('button').click();
+      },
+    () => {
+      // founction count
+      $('button#count').click();
+      },
+    () => {
+      // click field
+      $('div[data-test-subj="indexPattern-dimension-field"]').find('input').click();
+      },
+    () => {
+      const inputElement = $('div[data-test-subj="indexPattern-dimension-field"]').find('input')[0];
+      setInputValue(inputElement, "client_ip");
+      },
+    () => {
+      // click client_ip in drop down
+      $(".euiPopover__panel").find('button.euiFilterSelectItem[title="client_ip"]').click();
+      },
+    () => {
+      // close popup
+      $('span.euiButtonEmpty__text:contains("Close")').closest('button').click()
+      },
+
+
+    //----
+    //#### Breakdown
+    () => {
+      // edit Breakdown
+      $('fieldset').has('legend:contains("Breakdown")').find('button[title^="Edit"][title$="configuration"], button[aria-label="Add or drag-and-drop a field to Breakdown"]').click();
+      },
+    () => {
+      // click top values function
+      $('button#terms').click();
+      },
+    () => {
+      // click field
+      $('div[data-test-subj="indexPattern-dimension-field"]').find('input').click();
+      },
+    () => {
+      const inputElement = $('div[data-test-subj="indexPattern-dimension-field"]').find('input')[0];
+      setInputValue(inputElement, "user_agent.keyword");
+      },
+    () => {
+      // click client_ip in drop down
+      $(".euiPopover__panel").find('button.euiFilterSelectItem[title="user_agent.keyword"]').click();
+      },
+    () => {
+      const inputElement = $('input[aria-label="Number of values"]')[0];
+      setInputValue(inputElement, 10);
+      },
+    () => {
+      // close popup
+      $('span.euiButtonEmpty__text:contains("Close")').closest('button').click()
+      },
+];
+
+// Run the function to perform actions with 500ms steps
+stepTimeout(actions, 500);
